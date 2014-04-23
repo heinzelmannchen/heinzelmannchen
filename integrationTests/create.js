ec7a6cc387ac3e3783ea8234b0eef8c665ec32ae
Christoph Hermann <schtoeffel@gmail.com>
2014-04-23 15:35:05 +0200

    adds override default

--- integrationTests/create.js
+++ integrationTests/create.js
@@ -1,5 +1,7 @@
 var heinzel = require('../heinzel');
-heinzel.create('extViews.formView').then(function() {
+heinzel.create('extViews.formView', {
+    override: true
+}).then(function() {
     console.log('done');
 }).fail(function() {
     console.log(arguments);
