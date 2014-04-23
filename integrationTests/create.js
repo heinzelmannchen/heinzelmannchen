8f52897be469a315eaf5a4a2818d1390927967b5
Christoph Hermann <schtoeffel@gmail.com>
2014-04-23 17:09:18 +0200

    adds result list of filenames

--- integrationTests/create.js
+++ integrationTests/create.js
@@ -1,8 +1,8 @@
 var heinzel = require('../heinzel');
 heinzel.create('extViews.formView', {
     override: true
-}).then(function() {
-    console.log('done');
+}).then(function(files) {
+    console.log('done', files);
 }).fail(function() {
     console.log(arguments);
 });
