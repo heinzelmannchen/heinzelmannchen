88bec83ea80921bf9fdad899f5043395a73efe6f
Christoph Hermann <schtoeffel@gmail.com>
2014-04-26 15:30:02 +0200

    ensurePath

--- integrationTests/create.js
+++ integrationTests/create.js
@@ -1,6 +1,7 @@
 var heinzel = require('../heinzel');
 heinzel.create('extViews.formView', {
-    override: true
+    override: true,
+    ensurePathExists: true
 }).then(function(files) {
     console.log('done', files);
 }).fail(function() {
