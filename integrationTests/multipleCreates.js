1c37ca84102d5dde59f489178c0039750387a54d
sprengr <sprengr@outlook.com>
2014-05-01 10:51:36 +0200

    another test

---
+++ integrationTests/multipleCreates.js
@@ -0,0 +1,20 @@
+﻿var heinzel = require('../heinzel'),
+    Q = require('q'),
+    i,
+    promises = [];
+
+for (i=0;i<100;i++){
+    promises.push(heinzel.create('extViews.formView', {
+        override: true,
+        ensurePathExists: true,
+        filters: {
+            tables: ['heinzel', 'occupation']
+        }
+    }));
+}
+Q.allSettled(promises).then(function (files) {
+    console.log('done', files);
+}).fail(function () {
+    console.trace();
+    console.log(arguments);
+});
