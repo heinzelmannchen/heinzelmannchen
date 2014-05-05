59822732ba6714f016832999338b234c18cf8d7e
sprengr <sprengr@outlook.com>
2014-05-05 14:22:44 +0200

    fixed tests

--- test/install.js
+++ test/install.js
@@ -45,81 +45,79 @@ describe('install', function() {
 
     describe('#installGenerators', function () {
         var install,
-            installSpy = createSinonStubPromise(),
-            configSpy = createSinonStubPromise();
+            installStub = createSinonStubPromise(),
+            configStub = createSinonStubPromise();
 
         beforeEach(function () {
             install = new Install(_.extend(injections, {
                 npm: {
-                    install: installSpy
+                    install: installStub
                 },
                 requireHelper: new RequireHelper(injections),
                 heinzelConfig: {
-                    saveLocal: configSpy
+                    saveLocal: configStub
                 }
             }));
         });
         it('should call npm.install with the generator prefix', function () {
             install.installGenerators('module');
-            installSpy.should.have.been.calledWith(['heinzelmannchen-gen-module']);
+            installStub.should.have.been.calledWith(['heinzelmannchen-gen-module']);
         });
 
         it('should call npm.install with the generator prefix for multiple generators', function () {
             install.installGenerators(['module1', 'module2', 'module3']);
-            installSpy.should.have.been.calledWith(['heinzelmannchen-gen-module1', 'heinzelmannchen-gen-module2', 'heinzelmannchen-gen-module3']);
+            installStub.should.have.been.calledWith(['heinzelmannchen-gen-module1', 'heinzelmannchen-gen-module2', 'heinzelmannchen-gen-module3']);
         });
 
         it('shouldn\'t call heinzel config if no save flag is used', function () {
-            install.installGenerators('module');
-            return configSpy.should.not.have.been.called;
+            return install.installGenerators('module').then(function () {
+                return configStub.should.not.have.been.called;
+            });
         });
 
-        it.skip('should call heinzel config with the generator string', function () {
-            install.installGenerators('module', true);
-            return configSpy.should.have.been.calledWith('generators.heinzelmannchen-gen-module.npm');
+        it('should call heinzel config with the generator string', function () {
+            return install.installGenerators('module', true).then(function () {
+                return configStub.should.have.been.calledWith('generators.heinzelmannchen-gen-module.npm');
+            });
         });
     });
 
     describe('#installTemplates', function () {
         var install,
-            installSpy = createSinonStubPromise(),
-            configSpy = sinon.stub().returns((function () {
-                var q = Q.defer();
-                q.resolve();
-                return q.promise;
-            }()));
+            installStub = createSinonStubPromise(),
+            configStub = createSinonStubPromise();
 
         beforeEach(function () {
             install = new Install(_.extend(injections, {
                 npm: {
-                    install: installSpy
+                    install: installStub
                 },
                 requireHelper: new RequireHelper(injections),
                 heinzelConfig: {
-                    saveLocal: configSpy
+                    saveLocal: configStub
                 }
             }));
         });
         it('should call npm.install with the template prefix', function () {
             install.installTemplates('module');
-            installSpy.should.have.been.calledWith(['heinzelmannchen-tpl-module']);
+            installStub.should.have.been.calledWith(['heinzelmannchen-tpl-module']);
         });
 
         it('should call npm.install with the template prefix for multiple templates', function () {
             install.installTemplates(['module1', 'module2', 'module3']);
-            installSpy.should.have.been.calledWith(['heinzelmannchen-tpl-module1', 'heinzelmannchen-tpl-module2', 'heinzelmannchen-tpl-module3']);
+            installStub.should.have.been.calledWith(['heinzelmannchen-tpl-module1', 'heinzelmannchen-tpl-module2', 'heinzelmannchen-tpl-module3']);
         });
 
         it('shouldn\'t call heinzel config if no save-flag is used', function () {
-            install.installTemplates('module');
-            return configSpy.should.not.have.been.called;
+            return install.installTemplates('module').then(function () {
+                return configStub.should.not.have.been.called;
+            });
         });
 
-        it.skip('should call heinzel config with the template string', function () {
-            install.installTemplates('module', true);
-            configSpy.should.have.been.calledOnce();
-            //return configSpy.should.have.been.calledWith('templates.heinzelmannchen-tpl-module.npm');
-            console.dir(configSpy);
+        it('should call heinzel config with the template string', function () {
+            return install.installTemplates(['module'], true).then(function () {
+                return configStub.should.have.been.calledWith('templates.heinzelmannchen-tpl-module.npm');
+            });
         });
     });
     
